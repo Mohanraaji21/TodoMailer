6f14400dd7a82a4f8665e8cc51e54ca104d1e782
MohyJake21 <mohanrajib21@gmail.com>
2025-01-27 12:56:37 +0530

    Create and Get all Tasks
    Create and Get all tasks from backend complete

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,2 +1,29 @@
 const express = require('express');
+const Task = require('../models/Task');
+
+const router = express.Router();
+
+//Create Task
+router.post('/', async(req, res)=>{
+    try {
+        
+        const {title, status, dueDate} = req.body;
+        const task = await Task.create({title, status, dueDate});
+        res.status(201).json(task);
+
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+});
+
+//View Task
+router.get('/', async(req, res)=>{
+    try {
+        const tasks = await Task.find();
+        res.status(200).json(tasks);
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+});
+
 
